d495cf68d5a738eff0d876c635fa28d8701e9495
Aitor Moruno Cuenca <aitor.morunocuenca@almirall.com>
2025-03-31 09:13:41 +0000

    Remove option "packages = "singIST"" in furrr_options as it does not work

--- R/helpers.R
+++ R/helpers.R
@@ -309,8 +309,7 @@ execute_parallel_cv <- function(K, cores, results_CV_summary_n,
         n_quantile_comb = nrow(quantile.comb.table), Method = Method,
         measure = measure,expected.measure.increase = expected.measure.increase,
         center = center, scale = scale, maxiter = maxiter, .progress = TRUE,
-        .options = furrr::furrr_options(globals = FALSE, seed = TRUE,
-        packages = "singIST")
+        .options = furrr::furrr_options(globals = FALSE, seed = TRUE)
     )
     future::plan(sequential)  # Reset to sequential execution
     results_CV_summary_n <- output[[1]]$results_CV_summary_n
