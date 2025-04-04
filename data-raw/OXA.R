e38fd82a5ff1ab6c7d8d6d5b3299ae7d9d9063e7
Aitor Moruno Cuenca <amoruno@almirall.com>
2025-04-04 18:06:16 +0200

    Reduce OXA object memory usage

--- data-raw/OXA.R
+++ data-raw/OXA.R
@@ -1,6 +1,7 @@
 ## code to prepare `OXA_IMQ` dataset goes here
 library(readr)
 library(Seurat)
+librar(dplyr)
 # Load Seurat object with the disease model data
 raw_OXA_IMQ <- readRDS("data-raw/OXA_IMQ.rds")
 raw_OXA_IMQ <- Seurat::UpdateSeuratObject(raw_OXA_IMQ)
@@ -9,11 +10,10 @@ OXA <- raw_OXA_IMQ[, raw_OXA_IMQ$stim %in% c("OXA", "ETOH")]
 OXA <- OXA[, OXA$identities %in% c("cDC2", "cDC1", "migratory DCs",
                                     "Keratinocytes", "LC", "DETC", "dγdT", "T")]
 # Subsample cells and genes of the object to reduce memory 
-set.seed(123)
-n_cells <- round(ncol(OXA)/22, 0)
-n_genes <- round(nrow(OXA)/3, 0)
-subsample_cells <- sample(colnames(OXA), n_cells)
-subsample_genes <- sample(rownames(OXA), n_genes)
-OXA <- subset(OXA, features = subsample_genes)
-OXA <- subset(OXA, cells = subsample_cells)
+# Sample 20 cells per cell type + condition combination
+meta <- OXA@meta.data  %>% mutate(cell_id = rownames(.))
+sampled_cells <- meta %>% group_by(identities, stim)  %>% slice_sample(n = 20) %>% pull(cell_id)
+sampled_genes <- sample(rownames(OXA), round(nrow(OXA)/2, 0))
+OXA <- subset(OXA, features = sampled_genes)
+OXA <- subset(OXA, cells = sampled_cells)
 usethis::use_data(OXA, compress = "xz", overwrite = TRUE)
