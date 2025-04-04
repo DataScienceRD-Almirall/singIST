d65106cac9a70a4b42c7f24cfe828dd81c32060d
Aitor Moruno Cuenca <amoruno@almirall.com>
2025-04-04 17:07:43 +0200

    Subset cells and genes of OXA toy dataset to reduce memory usage

--- data-raw/OXA.R
+++ data-raw/OXA.R
@@ -8,4 +8,12 @@ raw_OXA_IMQ <- Seurat::UpdateSeuratObject(raw_OXA_IMQ)
 OXA <- raw_OXA_IMQ[, raw_OXA_IMQ$stim %in% c("OXA", "ETOH")]
 OXA <- OXA[, OXA$identities %in% c("cDC2", "cDC1", "migratory DCs",
                                     "Keratinocytes", "LC", "DETC", "dγdT", "T")]
+# Subsample cells and genes of the object to reduce memory 
+set.seed(123)
+n_cells <- round(ncol(OXA)/22, 0)
+n_genes <- round(nrow(OXA)/3, 0)
+subsample_cells <- sample(colnames(OXA), n_cells)
+subsample_genes <- sample(rownames(OXA), n_genes)
+OXA <- subset(OXA, features = subsample_genes)
+OXA <- subset(OXA, cells = subsample_cells)
 usethis::use_data(OXA, compress = "xz", overwrite = TRUE)
